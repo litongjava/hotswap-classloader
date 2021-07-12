c40e8770b8752d794d793f3865b5faef505a66c7
litongjava <litongjava@qq.com>
2021-07-13 00:09:00 +0800

    取消不必要的watcher.poll()

--- src/main/java/com/litongjava/hotswap/classloader/HotSwapWatcher.java
+++ src/main/java/com/litongjava/hotswap/classloader/HotSwapWatcher.java
@@ -23,59 +23,58 @@ import com.litongjava.hotswap.server.RestartServer;
  * 监听 class path 下 .class 文件变动，触发 UndertowServer.restart()
  */
 public class HotSwapWatcher extends Thread {
-  
+
   protected RestartServer server;
-  
-  // protected int watchingInterval = 1000;  // 1900 与 2000 相对灵敏
+
+  // protected int watchingInterval = 1000; // 1900 与 2000 相对灵敏
   protected int watchingInterval = 500;
-  
+
   protected List<Path> watchingPaths;
   private WatchKey watchKey;
   protected volatile boolean running = true;
-  
+
   public HotSwapWatcher(RestartServer server) {
     setName("HotSwapWatcher");
-    // 避免在调用 deploymentManager.stop()、undertow.stop() 后退出 JVM 
+    // 避免在调用 deploymentManager.stop()、undertow.stop() 后退出 JVM
     setDaemon(false);
     setPriority(Thread.MAX_PRIORITY);
-    
+
     this.server = server;
     this.watchingPaths = buildWatchingPaths();
 
-
   }
-  
+
   protected List<Path> buildWatchingPaths() {
     Set<String> watchingDirSet = new HashSet<>();
     String[] classPathArray = System.getProperty("java.class.path").split(File.pathSeparator);
     for (String classPath : classPathArray) {
       buildDirs(new File(classPath.trim()), watchingDirSet);
     }
-    
+
     List<String> dirList = new ArrayList<String>(watchingDirSet);
     Collections.sort(dirList);
-    
+
     List<Path> pathList = new ArrayList<Path>(dirList.size());
     System.out.println("观察的目录有:");
     for (String dir : dirList) {
       System.out.println(dir);
       pathList.add(Paths.get(dir));
     }
-    
+
     return pathList;
   }
-  
+
   private void buildDirs(File file, Set<String> watchingDirSet) {
     if (file.isDirectory()) {
       watchingDirSet.add(file.getPath());
-      
+
       File[] fileList = file.listFiles();
       for (File f : fileList) {
         buildDirs(f, watchingDirSet);
       }
     }
   }
-  
+
   public void run() {
     try {
       doRun();
@@ -83,72 +82,60 @@ public class HotSwapWatcher extends Thread {
       throw new RuntimeException(e);
     }
   }
-  
+
   protected void doRun() throws IOException {
     WatchService watcher = FileSystems.getDefault().newWatchService();
-    System.out.println("获取到的文件观察器是:"+watcher);
+    System.out.println("获取到的文件观察器是:" + watcher);
     addShutdownHook(watcher);
-    
+
     for (Path path : watchingPaths) {
-      path.register(
-          watcher,
+      path.register(watcher,
           // StandardWatchEventKinds.ENTRY_DELETE,
-          StandardWatchEventKinds.ENTRY_MODIFY,
-          StandardWatchEventKinds.ENTRY_CREATE
-      );
+          StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_CREATE);
     }
-    
+
     while (running) {
       try {
-        // watchKey = watcher.poll(watchingInterval, TimeUnit.MILLISECONDS);  // watcher.take(); 阻塞等待
+        // watchKey = watcher.poll(watchingInterval, TimeUnit.MILLISECONDS); //
+        // watcher.take(); 阻塞等待
         // 比较两种方式的灵敏性，或许 take() 方法更好，起码资源占用少，测试 windows 机器上的响应
         watchKey = watcher.take();
-        
+
         if (watchKey == null) {
           // System.out.println(System.currentTimeMillis() / 1000);
-          continue ;
+          continue;
         }
-      } catch (Throwable e) {            // 控制台 ctrl + c 退出 JVM 时也将抛出异常
+      } catch (Throwable e) { // 控制台 ctrl + c 退出 JVM 时也将抛出异常
         running = false;
-        if (e instanceof InterruptedException) {  // 另一线程调用 hotSwapWatcher.interrupt() 抛此异常
-          Thread.currentThread().interrupt();  // Restore the interrupted status
+        if (e instanceof InterruptedException) { // 另一线程调用 hotSwapWatcher.interrupt() 抛此异常
+          Thread.currentThread().interrupt(); // Restore the interrupted status
         }
-        break ;
+        break;
       }
-      
+
       List<WatchEvent<?>> watchEvents = watchKey.pollEvents();
-       for(WatchEvent<?> event : watchEvents) {
-         Kind<?> kind = event.kind();
+      for (WatchEvent<?> event : watchEvents) {
+        Kind<?> kind = event.kind();
         String fileName = event.context().toString();
-        
-        System.out.println(watcher.toString()+"检测到文件修改"+kind.toString()+","+fileName);
+
+        System.out.println(watcher.toString() + "检测到文件修改" + kind.toString() + "," + fileName);
         if (fileName.endsWith(".class")) {
           if (server.isStarted()) {
             server.restart();
             resetWatchKey();
-            
-            while((watchKey = watcher.poll()) != null) {
-              // System.out.println("---> poll() ");
-              watchKey.pollEvents();
-              resetWatchKey();
-            }
-            
-            break ;
           }
         }
       }
-      
-      resetWatchKey();
-    }
+    }//-->end while
   }
-  
+
   private void resetWatchKey() {
     if (watchKey != null) {
       watchKey.reset();
       watchKey = null;
     }
   }
-  
+
   /**
    * 添加关闭钩子在 JVM 退出时关闭 WatchService
    * 
@@ -164,7 +151,7 @@ public class HotSwapWatcher extends Thread {
       }
     }));
   }
-  
+
   public void exit() {
     running = false;
     try {
@@ -173,7 +160,7 @@ public class HotSwapWatcher extends Thread {
       UndertowKit.doNothing(e);
     }
   }
-  
+
 //  public static void main(String[] args) throws InterruptedException {
 //    HotSwapWatcher watcher = new HotSwapWatcher(null);
 //    watcher.start();
